275bc0019a76aee3fa15dab905cd54cb378f6bfe
Kenzie Togami <ket1999@gmail.com>
2016-04-09 17:53:01 -0700

    Update spoon to be small and gold

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Phaser from "phaser";
-var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, "", {preload: preload, create: create, update: update});
 
 function preload() {
     game.load.image("spoon", "img/spoon.png");
 }
 let sprite;
 function create() {
-    sprite = game.add.sprite(0, 0, "spoon");
+    sprite = game.add.sprite(200, 200, "spoon");
+    sprite.scale.setTo(0.2, 0.2);
     sprite.anchor.setTo(0.5, 0.5);
+    sprite.tint = 0xe5f53b; //sorta gold
 }
 
 function update() {
